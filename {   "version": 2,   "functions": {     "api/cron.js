6f7c2669f9be7e7fb6d965e1cb29547c9e3ef3dd
mnadim4786-cmyk <m.nadim4786@gmail.com>
2026-05-17 21:06:40 +0700

    Create cron.js

---
+++ {   "version": 2,   "functions": {     "api/cron.js
@@ -0,0 +1,13 @@
+{
+  "version": 2,
+  "functions": {
+    "api/webhook.js": {
+      "memory": 512,
+      "maxDuration": 30
+    },
+    "api/cron.js": {
+      "memory": 512,
+      "maxDuration": 30
+    }
+  }
+}
